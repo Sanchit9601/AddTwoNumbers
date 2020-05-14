ff130450ea7c98cfba49a2dbd0a8d943e7aa7e37
Sanchit9601 <56028348+Sanchit9601@users.noreply.github.com>
2020-05-14 15:24:13 +0530

    mainadd.c

--- main.c
+++ main.c
@@ -3,11 +3,8 @@
 int main() {
   int a, b, sum;
   
-  printf("Enter first integer: ");
-  scanf("%d", &a);
-  printf("Enter second integer: ");
-  scanf("%d", &b);
-  
+  printf("Enter first and second integer: ");
+  scanf("%d %d",&a,&b);
   sum = a + b;
   
   printf("The sum of two numbers is %d\n", sum);
